c74f669ef476b9535648d96590052ab1077cb0f7
THURRA VENKATA DHANUSH NAIDU <dhanushnaidu679@gmail.com>
2026-06-17 20:31:29 +0530

    first commit

---
+++ employeeTask.js
@@ -0,0 +1,27 @@
+window.alert("Welcome to Employee Portal")
+
+var employeeName="dhanush"
+let employeeAge= 23
+const employeeCompanyName="stackly"
+
+console.log("Employee Name:", employeeName)
+console.log("Employee Age:", employeeAge)
+console.log("Company Name:", employeeCompanyName)
+
+let confirmMsg=confirm("Have you completed today's task?")
+console.log(confirmMsg)
+
+let empName=prompt("Enter your name")
+console.log(empName)
+
+document.writeln("welcome ",empName)
+
+console.warn("Task submitted successfully")
+
+console.error("Task submission failed")
+
+ employeeAge=25
+console.log("Employee Age:",employeeAge)
+
+
+
